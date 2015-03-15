bcfcb8e54e29cbd93f2a0e850db0cbac23ab5d66
Kierran McPherson <kierran.mcpherson@xero.com>
2015-03-15 15:46:20 +1300

    removed parameterized login, it was broken

--- Assets/config.sh
+++ Assets/config.sh
@@ -7,18 +7,9 @@ git pull
 
 CONFIG="/config"
 
+ # first time run copy config
 if [[ ! -f $CONFIG/config.ini ]]; then
   # copy the file to the config directory, making it if necessary
   mkdir -p $CONFIG
   cp -a /tmp/config.ini $CONFIG/config.ini
-fi
-
-if [[ -n ${USERNAME} ]]; then
-  echo "The username is: ${USERNAME}"
-  sed -i "s/couchpotato-user/${USERNAME}/g" $CONFIG/config.ini
-fi
-
-if [[ -n ${PASSWORD} ]]; then
-  echo "The password is: ${PASSWORD}"
-  sed -i "s/couchpotato-password/${PASSWORD}/g" $CONFIG/config.ini
 fi
